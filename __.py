238dbe015bd2c5f9ca1858e1b442e7f97c7ffa0b
Pavel <pavelgodx@gmail.com>
2023-04-29 02:18:01 +0300

    added _

---
+++ __.py
@@ -0,0 +1,31 @@
+import _
+
+_ = _._
+
+for _ in range(_):
+    print(_)
+
+_ = _
+print(_)
+
+
+def _(_):
+    return _
+
+
+_(_)
+
+
+class _:
+
+    def _____(_):
+        return _
+
+    def ___(_):
+        _ = _
+        return _
+
+
+_ = _()
+_.___
+_._____()
